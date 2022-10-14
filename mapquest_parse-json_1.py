531e6677c465b9cd7b136f43f5d2fd6c04ad6581
Dwin18 <93507376+Dwin18@users.noreply.github.com>
2022-10-14 07:28:13 +0000

    Add files via upload

--- mapquest_parse-json_1.py
+++ mapquest_parse-json_1.py
@@ -8,10 +8,10 @@ key = "MuCYlqbebDq2nmWK7sR6P5nAMue5pNGc"
 
 
 while True:
-    orig = input("Starting Location: ")
+    orig = input("\033[1;32m Starting Location:  \n")
     if orig == "quit" or orig == "q":
         break
-    dest = input("Destination: ")
+    dest = input("\033[1;32m Destination:  \n")
     if dest == "quit" or dest == "q":
         break
     url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest}) 
@@ -25,6 +25,7 @@ while True:
         print("Directions from " + (orig) + " to " + (dest))
         print("Trip Duration:   " + (json_data["route"]["formattedTime"]))
         print("Kilometers:      " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
+
         print("Fuel Used (Ltr): " + str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78)))
         print("=============================================")
         for each in json_data["route"]["legs"][0]["maneuvers"]:
